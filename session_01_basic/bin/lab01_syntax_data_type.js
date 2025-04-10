cab253dee7ca67473013204b90b46c7aa421ba11
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2025-04-10 12:14:58 +0000

    feat(session01): basic data type

--- session_01_basic/bin/lab01_syntax_data_type.js
+++ session_01_basic/bin/lab01_syntax_data_type.js
@@ -1,4 +1,26 @@
 "use strict";
 let isDone = false;
-console.log(isDone);
+console.log(`isDone = ${isDone} - ${typeof (isDone)}`);
+let decimal = 6;
+let hex = 0xf0000d;
+let bin = 0b101010;
+let octal = 0o744;
+console.log(`Decimal = ${decimal} - ${typeof (decimal)}`);
+console.log(`Hex     = ${hex} - ${typeof (hex)}`);
+console.log(`Bin     = ${bin} - ${typeof (bin)}`);
+console.log(`Octal   = ${octal} - ${typeof (octal)}`);
+let big = 100n;
+console.log(`Big int = ${big} - ${typeof (big)}`);
+let color = 'light blue';
+color = 'dark red';
+console.log(`Color   = ${color} - ${typeof (color)}`);
+let fullName = `Mr Buoi`;
+let age = 37;
+let sentence = "Hello, my name is " +
+    fullName +
+    ".\n\n" +
+    "I'll be " +
+    (age + 1) +
+    " years old next month.";
+console.log(`Sentencd = ${sentence} - ${typeof (sentence)}`);
 //# sourceMappingURL=lab01_syntax_data_type.js.map
--- session_01_basic/src/lab01_syntax_data_type.ts
+++ session_01_basic/src/lab01_syntax_data_type.ts
@@ -3,5 +3,40 @@
  */
 
 //? boolean
-let isDone : boolean = false;
-console.log(isDone);
+let isDone : boolean = false
+console.log(`isDone = ${isDone} - ${typeof(isDone)}`)
+
+//! try assign a number to isDone
+//! isDone = 1 => Type 'number' is not assignable to type 'boolean'.
+
+//? number 
+let decimal : number = 6          // integer
+let hex     : number = 0xf0000d   // hex      => is converted to decimal
+let bin     : number = 0b101010   // binary   => is converted to decimal
+let octal   : number = 0o744      // octal    => is converted to decimal
+
+console.log(`Decimal = ${decimal} - ${typeof(decimal)}`)
+console.log(`Hex     = ${hex} - ${typeof(hex)}`)
+console.log(`Bin     = ${bin} - ${typeof(bin)}`)
+console.log(`Octal   = ${octal} - ${typeof(octal)}`)
+
+let big     : bigint = 100n
+console.log(`Big int = ${big} - ${typeof(big)}`)
+
+//? string 
+let color   : string = 'light blue'
+color                = 'dark red'
+console.log(`Color   = ${color} - ${typeof(color)}`)
+
+let fullName: string = `Mr Buoi`;
+let age: number = 37;
+let sentence: string =
+  "Hello, my name is " +
+  fullName +
+  ".\n\n" +
+  "I'll be " +
+  (age + 1) +
+  " years old next month.";
+ 
+console.log(`Sentencd = ${sentence} - ${typeof(sentence)}`)
+
