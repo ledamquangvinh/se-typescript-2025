f56cc641a41ab16e0ac6298acd87139a62d60960
Le Dam Quang Vinh <s3818480@rmit.edu.vn>
2025-05-19 17:36:21 +0700

    init(singleton): fresh new branch

--- session_03_patterns/pattern_03_factory_method/src/ConcreteProduct2.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./IProduct";
-
-export class ConcreteProduct2 implements Product {
-    public operation(): string {
-        return '{Result of the ConcreteProduct2}';
-    }
-}
--- session_03_patterns/pattern_03_factory_method/src/IProduct.ts
+++
@@ -1,7 +0,0 @@
-/**
- * The Product interface declares the operations that all concrete products must
- * implement.
- */
-export interface Product {
-    operation(): string;
-}
